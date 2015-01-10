8847e4896e77b3d010f03af2c972fb8ddca70cda
Jason Kim <jasonkim@rhapsody.com>
2015-01-10 13:14:44 -0800

    added live reloading

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,17 @@
-var gulp  = require('gulp'),
-    mocha = require('gulp-mocha'),
-    exit  = require('gulp-exit');
+var gulp    = require('gulp'),
+    mocha   = require('gulp-mocha'),
+    nodemon = require('gulp-nodemon'),
+    exit    = require('gulp-exit');
 
 gulp.task('default', function() {});
+
+gulp.task('server', ['nodemon']);
+
+gulp.task('nodemon', function() {
+  nodemon({
+    script: 'bin/www',
+  }).on('restart');
+});
 
 gulp.task('test-once', function() {
   return gulp.src('test/**/**/*.js', {read: false})
