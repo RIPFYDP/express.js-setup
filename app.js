51c417818151075d9251cd1d85fb7045663bf554
Jason Kim <jasonkim@rhapsody.com>
2015-01-14 23:55:48 -0800

    New routes pattern

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var _ = require('lodash');
+var routes = require('./config/application/routes');
 
 var app = express();
 
@@ -23,8 +22,10 @@ app.use(cookieParser());
 app.use(require('node-compass')({mode: 'expanded'}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+// Initialize routes
+_.each(routes, function(route) {
+  app.use(route.base, route.router);
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ config/application/routes.js
@@ -0,0 +1,10 @@
+var indexRoutes = require('../../routes/index'),
+    usersRoutes = require('../../routes/users'),
+    routes      = [];
+
+routes = [
+  {router: indexRoutes, base: '/'},
+  {router: usersRoutes, base: '/users'}
+];
+
+module.exports = routes;
