206c41b58f340dc6794819e29455a1c755c71889
Jason Kim <jasonkim@rhapsody.com>
2015-01-15 21:31:55 -0800

    initial commit using swig

--- app.js
+++ app.js
@@ -5,12 +5,23 @@ var express      = require('express'),
     cookieParser = require('cookie-parser'),
     bodyParser   = require('body-parser'),
     _            = require('lodash'),
+    swig         = require('swig'),
     routes       = require('./config/application/routes'),
     app          = express();
 
 // view engine setup
+app.engine('html', swig.renderFile);
+
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'html');
+
+// Swig will cache templates for you, but you can disable
+// that and use Express's caching instead, if you like:
+app.set('view cache', false);
+// To disable Swig's cache, do the following:
+swig.setDefaults({ cache: false });
+// NOTE: You should always cache templates in a production environment.
+// Don't leave both of these to `false` in production!
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
-});
-
-/* Bootstrap page */
-router.get('/bootstrap-page', function(req, res) {
-  res.render('bootstrap-page', {});
+  res.render('application/index', { title: 'Express' });
 });
 
 module.exports = router;
