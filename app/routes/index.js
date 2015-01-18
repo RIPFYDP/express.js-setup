df47017150d4888600f02f9be9a3954d01a378cd
Jason Kim <jasonkim@rhapsody.com>
2015-01-18 10:02:43 -0800

    added log in and sign up page

--- app/routes/index.js
+++ app/routes/index.js
@@ -6,10 +6,30 @@ router.get('/', function(req, res) {
   var options;
 
   options = {
-    title: "Express MV*"
+    title: 'Express MV*'
   };
 
   res.render('application/index', options);
+});
+
+router.get('/log_in', function(req, res) {
+  var options;
+
+  options = {
+    title: 'Log in'
+  };
+
+  res.render('users/log_in', options);
+});
+
+router.get('/sign_up', function(req, res) {
+  var options;
+
+  options = {
+    title: 'Sign up'
+  };
+
+  res.render('users/sign_up', options);
 });
 
 module.exports = router;
