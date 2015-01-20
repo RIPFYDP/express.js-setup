151e0c6f5474ddb2b7b51486a26407b90a95f2fc
Jason Kim <jasonkim@rhapsody.com>
2015-01-20 01:00:00 -0800

    running mongodb client in tests

--- test/app/models/user_test.js
+++ test/app/models/user_test.js
@@ -1,11 +1,41 @@
 var chai   = require('chai'),
     expect = chai.expect,
     _      = require('lodash'),
-    User   = require('../../../app/models/user');
+
+    app           = require('../../../app'),
+    http          = require('http'),
+    globalLibrary = require('../../../config/application/global_library'),
+    Mongo         = require('../../../config/db/mongo'),
+    mongo         = new Mongo(),
+    mongoOptions  = {
+                      server: {
+                        poolSize: 1
+                      }
+                    },
+
+    User   = require('../../../app/models/user'),
+    port   = 3002,
+    server;
+
+before(function(done) {
+
+  app.set('port', port);
+
+  server = http.createServer(app);
+  mongo.connect(mongoOptions, function(){
+    globalLibrary = mongo.db;
+    server.listen(port);
+    done();
+  });
+
+});
 
 describe('app/models/user', function() {
   it('.all', function() {
     var users = User.all();
+
+    // mongo.db is now available.
+    // console.log(mongo.db);
 
     expect(users).to.be.a('Array');
   });
