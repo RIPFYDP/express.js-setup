a785f3187729697ec39f6b4005169b9d26b880d0
Jason Kim <jasonkim@rhapsody.com>
2015-01-31 12:51:11 -0800

    test timeout to 60000

--- test/app/views/users/sign_up_html_test.js
+++ test/app/views/users/sign_up_html_test.js
@@ -9,7 +9,7 @@ var app       = require('../../../../app'),
     nightmare;
 
 describe('/sign_up', function() {
-  this.timeout(30000);
+  this.timeout(60000);
 
   before(function(done) {
     server = app.listen(3001);
